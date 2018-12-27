1c39c5b99c0859901dafda452ab7d90c4e0f82a8
Anthony Dupont <Frenchy2199@yahoo.com>
2018-12-27 00:08:21 -0700

    Created the game and dungeon class

--- DungeonCrawl/Game.h
+++ DungeonCrawl/Game.h
@@ -2,6 +2,8 @@
 #ifndef GAMES_H
 #define GAMES_H
 
+class Dungeon;
+
 enum class EGameStatus
 {
 	Invalid_guess,
@@ -9,6 +11,28 @@ enum class EGameStatus
 	EndGame
 };
 
+class Game
+{
+private:
+	bool gameOver;
+	Dungeon dungeonRoom;
+	int maxRooms;	//keeps track of how many rooms there is supposed to be during the game
+public:
+	Game();
+	void setGameOver(bool _gameOver) { gameOver = _gameOver; }
+	void setMaxRooms(int _maxRooms) { maxRooms = _maxRooms; }
+
+	bool getGameOver() { return gameOver; }
+	int getMaxRooms() { return maxRooms; }
+};
+
+class Dungeon
+{
+private:
+
+public:
+	Dungeon();
+};
 
 #endif // !GAMES_H
 
