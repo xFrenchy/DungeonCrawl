85b1bcabd3e645cd85986d1d4edb221b99c29ad0
Anthony Dupont <Frenchy2199@yahoo.com>
2019-01-06 23:52:59 -0700

    Added comments

--- DungeonCrawl/Items.h
+++ DungeonCrawl/Items.h
@@ -1,3 +1,5 @@
 #pragma once
 //TODO create a class or struct for objects to make a better inventory since the inventory is strings
-//"Fish", "Lobster", "Minion Meat", "Shark", "Dragon Meat"
+//int treasuresGold[] = { 25, 50, 100, 250, 500, 1000 };	//6 items
+//std::string treasureStatBoost[] = { "+1 Attack", "+3 Attack", "+5 Attack", "+1 Strength", "+3 Strength", "+5 Strength", "+1 Defence", "+3 Defence", "+5 Defence" };	//9 items
+//std::string treasureFood[] = { "Fish", "Lobster", "Minion Meat", "Shark", "Dragon Meat" };	//5 items
