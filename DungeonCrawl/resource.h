708b94235cdcd1b910189ee41e80e0739ed63a02
Anthony Dupont <Frenchy2199@yahoo.com>
2019-01-13 22:53:16 -0700

    Newer version, fixed issue with no sound

---
+++ DungeonCrawl/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDR_WAVE2                       105
+#define IDR_WAVE1                       106
+#define IDR_WAVE3                       108
+#define IDR_WAVE4                       109
+#define IDR_WAVE5                       110
+#define IDR_WAVE6                       111
+#define IDR_WAVE7                       112
+#define IDR_WAVE8                       113
+#define IDR_WAVE9                       114
+#define IDR_WAVE10                      115
+#define IDR_WAVE11                      116
+#define IDR_WAVE12                      117
+#define IDR_WAVE13                      118
+#define IDR_WAVE14                      119
+#define IDR_WAVE15                      120
+#define IDR_WAVE16                      121
+#define IDR_WAVE17                      122
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        123
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
